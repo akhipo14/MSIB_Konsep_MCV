eebd60267f3c57c15049deffd28fbb6d6ac581b4
akhipo14 <akhipo14@gmail.com>
2023-03-09 20:51:23 +0700

    update css

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\ProdukController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('produk',[ProdukController::class,'index']);
+Route::get('/',[ProdukController::class,'index']);
